c11b3611ad057564551a2b8ef2d80bec032d1cd1
nzitha023 <nzitha023@student.wethinkcode.co.za>
2024-07-26 10:12:25 +0200

    Added a categories file

---
+++ categories.py
@@ -0,0 +1,16 @@
+import requests
+from pprint import pprint
+import json
+
+
+categories_url = "https://opentdb.com/api_category.php"
+response = requests.get(categories_url)
+print(response)
+
+#Getting response in json format and pretty printing it.
+print(response.json())
+pprint(response.json())
+
+# Dump the responses in a json file
+with open("categories.json","w") as f:
+    json.dump(response.json(),f,indent=4)
